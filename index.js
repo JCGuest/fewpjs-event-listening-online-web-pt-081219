adcc31b1459a43290c85990441fb8be511627c0a
JCGuest <jcguest90@gmail.com>
2020-02-27 23:00:31 -0600

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const input = document.getElementById("input")
+// input.addEventListener('click', function(event) {
+//     alert('I was clicked, ooooooo-wee!')
+// });
+
 function addingEventListener() {
+    input.addEventListener('click', function(event) {
+        alert('I was clicked, ooooooo-wee!')
+    });
 }
